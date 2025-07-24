422770b57370bc681a69b3befac39a2337130f8f
Ashish manhas <79656682+iamashishxo@users.noreply.github.com>
2025-07-24 07:18:20 +0200

    Update SecurityConfig.java

--- src/main/java/com/UpStrives/upstrives/config/SecurityConfig.java
+++ src/main/java/com/UpStrives/upstrives/config/SecurityConfig.java
@@ -14,7 +14,7 @@ public class SecurityConfig {
         http
           .csrf(csrf -> csrf.disable())
           .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/api/**").permitAll()   // 👈 open your APIs
+                .requestMatchers("/api/**").permitAll()   
                 .anyRequest().authenticated())
           .sessionManagement(sm ->
                 sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
